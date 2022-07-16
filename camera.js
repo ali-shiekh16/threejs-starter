69d83638cba367d40e2a9f17109dc89d2d39684c
ali <ali.shiekh16@outlook.com>
2022-07-16 15:06:57 +0500

    create basic solar system.

--- camera.js
+++ camera.js
@@ -1,10 +1,11 @@
-import { PerspectiveCamera } from 'three';
+import { PerspectiveCamera, Vector3 } from 'three';
 import configs from './configuration';
 
 const { width, height } = configs.sizes;
 
 const camera = new PerspectiveCamera(45, width / height);
-camera.position.setZ(20);
+camera.position.setY(20);
+camera.lookAt(new Vector3(0, 0, 0));
 
 function updateCamera(width, height) {
   camera.aspect = width / height;
